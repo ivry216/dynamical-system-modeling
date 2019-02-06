561890dea4a79c3836847082cfdb873d67274984
IvanRyzhikov <ivan.s.ryzhikov@gmail.com>
2019-02-06 23:27:25 +0200

    Add IO for modeling results
    Add Interface for IO manager of modeling results

---
+++ LdeModeling/Models/ModelingResults/Dynamical/DynamicalModelResultsIOManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApp.Models.Dynamical;
+
+namespace TestApp.Models.ModelingResults.Dynamical
+{
+    public class DynamicalModelResultsIOManager
+        : IModelingResultsIOManager<DiscreteDynamicalModelOutput, DiscreteDynamicalModelInput>
+    {
+        public void Save(DiscreteDynamicalModelOutput modelOutput, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(DiscreteDynamicalModelOutput modelOutput, DiscreteDynamicalModelInput modelInput, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LdeModeling/Models/ModelingResults/IModelingResultsIOManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Models.ModelingResults
+{
+    public interface IModelingResultsIOManager<TModelOutput, TModelInput>
+        where TModelOutput : IModelOutput
+        where TModelInput : IModelInput
+    {
+        void Save(TModelOutput modelOutput, string fileName);
+        void Save(TModelOutput modelOutput, TModelInput modelInput, string fileName);
+    }
+}
