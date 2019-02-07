bcf7a20817d8357a238fa099994d35fff6669ac1
IvanRyzhikov <ivanry@uef.fi>
2019-02-07 18:08:19 +0200

    Implement interface of saving model parameters to file
    Implement generic interface for saving to file the model parameters

---
+++ LdeModeling/Models/IOManagers/Parameters/IParametersIOManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Models.IOManagers.Parameters
+{
+    public interface IParametersIOManager<TParameters>
+        where TParameters : IModelParameters
+    {
+        void Save(TParameters modelParameters, string fileName);
+    }
+}
