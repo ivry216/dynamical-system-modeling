c20c694b03b3dd0b7841052c7740f092730bd8de
IvanRyzhikov <ivanry@uef.fi>
2019-02-07 17:21:57 +0200

    Add saving the inputs to dynamical model output
    Implement method that saves the time points, dynamical model outputs and the inputs.

--- LdeModeling/Models/ModelingResults/Dynamical/DynamicalModelResultsIOManager.cs
+++ LdeModeling/Models/ModelingResults/Dynamical/DynamicalModelResultsIOManager.cs
@@ -51,7 +51,52 @@ namespace TestApp.Models.ModelingResults.Dynamical
 
         public void Save(DiscreteDynamicalModelOutput modelOutput, DiscreteDynamicalModelInput modelInput, string fileName)
         {
-            throw new NotImplementedException();
+            // Save the data to file
+            using (ExcelPackage package = new ExcelPackage(new FileInfo(fileName)))
+            {
+                // Create worksheet
+                ExcelWorksheet wsData = package.Workbook.Worksheets.Add("Data");
+                // Set column names
+                int rowIndex = 1;
+                wsData.Cells[rowIndex, 1].Value = "Time";
+                for (int i = 2; i <= modelOutput.NumberOfOutputs + 1; i++)
+                {
+                    wsData.Cells[rowIndex, i].Value = i;
+                }
+                for (int i = modelOutput.NumberOfOutputs + 2, j = 1; i <= modelOutput.NumberOfOutputs + modelInput.NumberOfInputs + 1; i++, j++)
+                {
+                    wsData.Cells[rowIndex, i].Value = i;
+                }
+
+                // Save the data
+                // Save timepoints
+                int colIndex = 1; // Index for column containing time
+                rowIndex++;
+                for (int i = 0, j = rowIndex; i < modelOutput.Count; i++, j++)
+                {
+                    wsData.Cells[j, colIndex].Value = modelOutput.Times[i];
+                }
+                // Save output values
+                colIndex = 2; // Index for column containing time
+                for (int i = 0, row = rowIndex; i < modelOutput.Count; i++, row++)
+                {
+                    for (int output = 0, col = colIndex; output < modelOutput.NumberOfOutputs; output++, col++)
+                    {
+                        wsData.Cells[row, col].Value = modelOutput.Outputs[i][output];
+                    }
+                }
+                // Save input values
+                colIndex = 2 + modelOutput.NumberOfOutputs; // Index for column containing time
+                for (int i = 0, row = rowIndex; i < modelOutput.Count; i++, row++)
+                {
+                    for (int input = 0, col = colIndex; input < modelInput.NumberOfInputs; input++, col++)
+                    {
+                        wsData.Cells[row, col].Value = modelInput.Inputs[i][input];
+                    }
+                }
+
+                package.Save();
+            }
         }
     }
 }
