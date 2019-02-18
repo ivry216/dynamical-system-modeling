e664755053c42d979360ce3fd81316e139edaced
IvanRyzhikov <ivanry@uef.fi>
2019-02-18 23:49:49 +0200

    Add classes for stats collecting
    Add the main classes that wraps the results of algorithm runs

---
+++ LdeModeling/Optimization/AlgorithmsControl/AlgorithmRunStatistics/BestSolutionStatsReal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApp.MathematicalCore.ArrayExtensions;
+
+namespace TestApp.Optimization.AlgorithmsControl.AlgorithmRunStatistics
+{
+    public class BestSolutionStatsReal : BestSolutionStats<double, double[]>
+    {
+        public BestSolutionStatsReal(double best, double[] bestSolution) : base(best, bestSolution)
+        { }
+
+        protected override void Initialize(double best, double[] bestSolution)
+        {
+            BestValue = best;
+            BestSolution = new double[bestSolution.Length];
+            BestSolution.FillWithVector( bestSolution);
+        }
+    }
+}
---
+++ LdeModeling/Optimization/AlgorithmsControl/IOptimizationLauncherResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApp.Optimization.AlgorithmsControl.AlgorithmRunStatistics;
+
+namespace TestApp.Optimization.AlgorithmsControl
+{
+    public interface IOptimizationLauncherResult<TBest, TBestSolution, TRunStats>
+        where TRunStats : IAlgorithmRunCompositeStats<TBest, TBestSolution>
+    {
+        int Count { get; }
+        TRunStats[] Stats { get; }
+    }
+}
