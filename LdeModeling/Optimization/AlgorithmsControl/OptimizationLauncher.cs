db223c6f0d4768019dd62945d9c7c1e4b8806df4
IvanRyzhikov <ivan.s.ryzhikov@gmail.com>
2019-05-09 15:29:22 +0300

    Clean usings

--- LdeModeling/Optimization/AlgorithmsControl/OptimizationLauncher.cs
+++ LdeModeling/Optimization/AlgorithmsControl/OptimizationLauncher.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TestApp.Optimization.AlgorithmsControl.InformationCollectingManager;
+﻿using TestApp.Optimization.AlgorithmsControl.InformationCollectingManager;
 
 namespace TestApp.Optimization.AlgorithmsControl
 {
--- LdeModeling/Optimization/LocalOptimization/RandomCoordinatewiseOptimizator.cs
+++ LdeModeling/Optimization/LocalOptimization/RandomCoordinatewiseOptimizator.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TestApp.MathematicalCore.ArrayExtensions;
+﻿using TestApp.MathematicalCore.ArrayExtensions;
 using TestApp.MathematicalCore.Randomizing;
-using TestApp.Optimization.Problem;
 
 namespace TestApp.Optimization.LocalOptimization
 {
