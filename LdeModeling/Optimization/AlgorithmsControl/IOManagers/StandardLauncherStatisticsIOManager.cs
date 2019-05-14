9977c7706731dc97a449cc5ee49c10a848aab3b8
Ivan <ivan.s.ryzhikov@gmail.com>
2019-05-14 23:23:45 +0300

    Add basics for stats saving functional

---
+++ LdeModeling/Optimization/AlgorithmsControl/IOManagers/StandardLauncherStatisticsIOManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TestApp.Optimization.AlgorithmsControl.AlgorithmMeta;
+using TestApp.Optimization.AlgorithmsControl.AlgorithmRunStatistics;
+
+namespace TestApp.Optimization.AlgorithmsControl.IOManagers
+{
+    public class StandardLauncherStatisticsIOManager : ITestSessionResultsIOManager<IContainingStats<IBestVariableAndValueStats>, IBestVariableAndValueStats>
+    {
+        public void SaveStats(IContainingStats<IBestVariableAndValueStats> launcher)
+        {
+            // Assign the stats to the new variable
+            var stats = launcher.Stats;
+
+            //
+            throw new NotImplementedException();
+        }
+    }
+}
