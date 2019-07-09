892f5cb3a65d7b6ae072e24f85f3890a1b4ec1b7
IvanRyzhikov <ivan.s.ryzhikov@gmail.com>
2019-07-09 22:08:40 +0300

    Add collectors to restart meta

---
+++ Optimization/AlgorithmsControl/Restart/Conditional/Collectors/BestValueAndVariableRestartCollector.cs
@@ -0,0 +1,37 @@
+﻿using Optimization.AlgorithmsControl.AlgorithmRunStatistics;
+using System.Collections.Generic;
+
+namespace Optimization.AlgorithmsControl.Restart.Conditional.Collectors
+{
+    public class BestValueRestartCollector
+    {
+        private int _windowSize;
+        private Queue<double> _bestValues;
+
+        public IReadOnlyCollection<double> BestValues => _bestValues;
+
+        public BestValueRestartCollector(int windowSize)
+        {
+            _windowSize = windowSize;
+            _bestValues = new Queue<double>();
+        }
+
+        public void AddBest(double bestValue)
+        {
+            if (_bestValues.Count < _windowSize)
+            {
+                _bestValues.Enqueue(bestValue);
+            }
+            else
+            {
+                _bestValues.Dequeue();
+                _bestValues.Enqueue(bestValue);
+            }
+        }
+
+        public void Reset()
+        {
+            _bestValues.Clear();
+        }
+    }
+}
---
+++ Optimization/AlgorithmsControl/Restart/Conditional/Collectors/BestVariableRestartCollector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Optimization.AlgorithmsControl.Restart.Conditional.Collectors
+{
+    public class BestVariableRestartCollector
+    {
+        private Queue<double[]> _bestVariables;
+
+        public IReadOnlyCollection<double[]> BestVariables => _bestVariables;
+
+        public BestVariableRestartCollector()
+        {
+            _bestVariables = new Queue<double[]>();
+        }
+
+        public void AddBestAlternative(double[] bestAlternative)
+        {
+            _bestVariables.Enqueue(bestAlternative);
+        }
+
+        public void Reset()
+        {
+            _bestVariables.Clear();
+        }
+    }
+}
