ed137a94fe60d456101792e187b2baa347cf7129
ivan.s.ryzhikov <ivan.s.ryzhikov@gmail.com>
2019-08-19 22:17:44 +0300

    Add optimization problem constrains interface and base class

---
+++ Optimization/Problem/Constrains/IConstrainer.cs
@@ -0,0 +1,11 @@
+﻿using Optimization.Problem.Parallel;
+
+namespace Optimization.Problem.Constrains
+{
+    public interface IConstrainer<TValues, TAlternatives>
+        where TValues : IParallelOptimizationProblemValues
+        where TAlternatives : IParallelOptimizationProblemAlternative
+    {
+        TValues Evaluate(TAlternatives alternatives);
+    }
+}
