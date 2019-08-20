4e16028faca8907a04fddfb3f19f8c23db3fc44f
ivan.s.ryzhikov <ivan.s.ryzhikov@gmail.com>
2019-08-20 14:12:12 +0300

    Add soft and hard constrainer

---
+++ Optimization/Problem/Constrains/Parallel/ParallelSoftAndHardConstrainer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Optimization.Problem.Constrains.Parallel
+{
+    public class ParallelSoftAndHardConstrainer : ParallelConstrainerBase
+    {
+        public ParallelSoftAndHardConstrainer(Func<double, double[]> objectiveFunction) : base(objectiveFunction)
+        {
+        }
+
+        public override double Evaluate(double[] alternatives)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
