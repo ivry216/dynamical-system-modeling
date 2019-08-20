5154c34d8cd9ee39b933da353c92967b0b177d4b
ivan.s.ryzhikov <ivan.s.ryzhikov@gmail.com>
2019-08-20 09:30:09 +0300

    Separate constrained problem

---
+++ Optimization/Problem/Parallel/Constrained/ParallelOptimizationConstrainedProblem.cs
@@ -0,0 +1,43 @@
+﻿using Optimization.Problem.Constrains.Parallel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimization.Problem.Parallel.Constrained
+{
+    public abstract class ParallelOptimizationConstrainedProblem<TValues, TAlternatives> : IParallelOptimizationProblem<TValues, TAlternatives>
+        where TValues : IParallelOptimizationProblemValues
+        where TAlternatives : IParallelOptimizationProblemAlternative
+    {
+        #region Fields
+
+        protected IParallelConstrainer<TValues, TAlternatives> _constrainer;
+
+        #endregion Fields
+
+        #region Properties
+
+        public int Dimension { get; }
+        public bool IsConstrained => true;
+
+        #endregion Properties
+
+        #region Constructor
+
+        public ParallelOptimizationConstrainedProblem(int dimension, IParallelConstrainer<TValues, TAlternatives> constrainer)
+        {
+            Dimension = dimension;
+            _constrainer = constrainer;
+        }
+
+        #endregion Construtor
+
+        #region Inherited Methods
+
+        public abstract TValues CalculateCriterion(TAlternatives alternatives);
+
+        #endregion Inherited Methods
+    }
+}
